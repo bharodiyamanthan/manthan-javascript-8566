dfcca57aa53902c7fc96dcf8f178cfb86882e965
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-11-29 19:07:48 +0530

    assignment 29-nov

---
+++ assignment.js
@@ -0,0 +1,95 @@
+// 1.program
+
+let unit;
+
+if(unit>=0 && unit<=100){
+    console.log('');
+}else if(unit>100 && unit<300){
+    console.log('60p per unit');
+}else if(unit>300){
+    console.log('80p per unit');
+}
+
+
+
+
+// 2. program 
+
+// let age;
+
+// age = prompt('enter your age');
+
+// if(age>=18){
+//     alert('person eligible for vote..!!');
+// }
+// else{
+//     alert('person can not eligible for vote');
+// }
+
+
+
+//  3.program
+
+// let salary;
+
+// salary = prompt('enter  your salary');
+
+// if(salary>=5000){
+//     console.log('hra salary is : and',salary*0.08);
+//     console.log('da salary is : and',salary*0.20);
+// }else if(salary>5000 && salary<10000){
+//     console.log('hra salary is : and',salary*0.12);
+//     console.log('da salary is : and',salary*0.30);
+// }else if(salary>10000 && salary<15000){
+//     console.log('hra salary is : and',salary*0.15);
+//     console.log('da salary is : and',salary*0.40);
+// }else if(salary>15000){
+//     console.log('hra salary is : and',salary*0.20);
+//     console.log('da salary is : and',salary*0.50);
+// }
+// else{
+//     console.log('salary does not existed')
+// }
+
+//4. program
+
+let income;
+
+income = Number(prompt('enter income'));
+
+if(income<=2500){
+    console.log('Tax Free');
+}else if(income>2500 && income<5000){
+    console.log('sleb2 is:',(income-2500)*0.10);
+}else if(income>5000 && income<10000){
+    console.log('sleb3 is:',((income-5000)*0.20 + 250));
+}else if(income>10000){
+    console.log('sleb4 is',income*0.30);
+}
+else{
+    console.log('income does not matched')
+}
+
+
+
+//5. program
+
+// let marks;
+
+// marks = prompt('enter your marks');
+
+// if(marks>=75){
+//     console.log('grade a');
+// }else if(marks>60 && marks<75){
+//     console.log('grade b');
+// }else if(marks>45 && marks<60){
+//     console.log('grade c');
+// }else if(marks>35 && marks<45){
+//     console.log('grade d');
+// }else if(marks<35){
+//     console.log('failed')
+// }
+
+//6. program
+
+
