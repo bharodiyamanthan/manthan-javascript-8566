ccee1a2bbf450e9d30565d0b8074c4059b56d314
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-03 19:03:03 +0530

    condition statement 3dec

---
+++ condition.js
@@ -0,0 +1,97 @@
+// 1.. print 10 to 1
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// 2. 51 to 79
+
+// for(let i=51; i<=79; ){
+//     console.log(i);
+// }
+
+
+// 3 enter n and print out 1 to n..
+
+// let n;
+
+// n= prompt("enter a number:");
+
+// for(i=0; i<=n; i++){
+//     console.log(i);
+// }
+
+
+//4 enter n and print out n to 1
+
+// let n;
+
+// n = prompt("enter a number :");
+
+// for(i=-n; i>=0; i--){
+//     console.log(i);
+// }
+
+
+//5 enter n and print out n to n
+// let n;
+
+// n = prompt ("enter a number;",n);
+
+// for(i=-n; i<=n; i++){
+//     console.log(i);
+// }
+
+
+//6 enter n and print odd number
+
+// let n;
+
+// n = prompt ("enter a number:");
+
+// for(i=0; i<=n; i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+//7 enter n and print even number
+
+// let n;
+
+// n = prompt ("enter a number:");
+
+// for(i=0; i<=n; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+//8    1,4,3,16,5 using for loop in javascript.......
+
+// let n;
+
+// n = prompt ("enter a number:");
+
+// for(i=1; i<=n; i++){
+//     if(i%2==0){
+//         console.log(i**2);
+//     }   
+//     if(i%2==1) 
+//     console.log(i);
+// }
+
+//9    1,1,2,3,5,8....
+
+let n;
+
+n = prompt ("enter  a number");
+
+for(i=1; i<=n; i++){
+    console.log();
+}
+
+
+//10 1,4,12,32......
