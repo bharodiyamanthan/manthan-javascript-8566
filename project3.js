dfd2a28ee058ae9ce1853bd9189173cb37404909
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-05 18:38:27 +0530

    project3 5-dec

---
+++ project3.js
@@ -0,0 +1,106 @@
+//1. write js program to check number is palindrom or not...
+
+    // let num;
+    //  num = prompt("enter a n number");
+    // let value =num;
+    // let revnum=0;
+    
+    // while(num > 0){
+    //     let digit = num % 10;
+    //     revnum = revnum * 10 + digit;
+    //     num = (num - (num % 10) / 10);
+    // }
+
+    // if(value == revnum){
+    //     console.log("number is ${value} palindrom number");
+    // }else{
+    //     console.log("number is not ${value } palindrom number");    
+    // }
+
+
+//****************************************************************************** */
+
+//2. print series 1,4,3,16,5 .....
+
+// let n;
+
+// n = prompt ("enter a number:");
+
+// for(i=1; i<=n; i++){
+//     if(i%2==0){
+//         console.log(i**2);
+//     }   
+//     if(i%2==1) 
+//     console.log(i);
+// }
+
+// output : 1,4,3,16,5..
+
+//******************************************************************************* */
+
+//3. print multiplication table of n...
+
+// let n ;
+
+// n = prompt("enter a number:");
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(n + "*" + i +"=" + n * i);
+// }
+
+// input : 10
+
+// output: 
+// 10*1=10
+// 10*2=20
+// 10*3=30
+// 10*4=40
+// 10*5=50
+// 10*6=60
+// 10*7=70
+// 10*8=80
+// 10*9=90
+// 10*10=100
+
+
+//***************************************************************************** */
+
+//4. find out sum of digit of given number
+
+// let n;
+
+// n = prompt("enter a number:");
+
+// let sum =0;
+
+// for(i=0; i<=n; i++){
+//     sum+=i;
+//     console.log("sum of given number is:",sum);
+// }
+
+//input :10
+//output : sum of given number is : 55
+
+
+//*************************************************************************** */
+
+//5. print series 1,1,2,3,5,8;;
+
+    // let n;
+
+    // n = prompt("enter a number:");
+    // let a=1,b=1;
+
+    // console.log(a);
+    // console.log(b);
+
+    // for(i=1; i<=n; i++){
+    //     c=a+b;
+    //     a=b;
+    //     b=c;
+    //     console.log(c);    
+    // }
+
+    //output: 1,1,2,3,5,8,13,21,34,55,81,144..
+                                                                                                              
+    
