361089d1dff2a692818f95e159c329b1b34c297a
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-12 16:29:33 +0530

    nested if 12/12/2024

---
+++ nested.js
@@ -0,0 +1,73 @@
+// nested if statment
+
+// var a = Number(prompt("enter a value"));
+// var b = Number(prompt("enter b value"));
+// var c = Number(prompt("enter c value"));
+// var d = Number(prompt("enter d value"));
+
+// if(a > b){
+//     if(a > c){
+//         if(a > d){
+//             console.log("a is big");
+//         }
+//         else{
+//             console.log("d is big");
+//         }
+//     }else{
+//         if(c > d){
+//             console.log("c is big");
+//         }
+//         else{
+//             console.log("d is big");
+//         }
+//     }
+// }
+// else{
+//     if(b > c){
+//        if(b > d){
+//         console.log("b is big");
+//        }
+//        else{
+//         console.log("d is big");
+//        }
+//     }
+//     else{
+//         if(c > d){
+//             console.log("c is big");
+//         }
+//         else{
+//             console.log("d is big");
+//         }
+//     }
+// }
+
+
+// // grede 
+
+// var s1 = Number(prompt("enter s1 marks"));
+// var s2 = Number(prompt("enter s2 marks"));
+// var s3 = Number(prompt("enter s3 marks"));
+// var s4 = Number(prompt("enter s4 marks"));
+
+// console.log("s1 marks=",s1);
+// console.log("s2 marks=",s2);
+// console.log("s3 marks=",s3);
+// console.log("s4 marks=",s4);
+
+// var total = s1+s2+s3+s4;
+// console.log("totalv marks is:",total);
+
+// var avg = total/4;
+// console.log("avg is",avg,"%");
+
+// if(avg > 35){
+//    if(grede >50){
+//     console.log("b grede");
+//    }
+//    else{
+//     console.log("c grede");
+//    }
+// }
+// else{
+//     console.log("you are fail");
+// }
