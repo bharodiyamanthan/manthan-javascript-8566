6d3064ce7ba03b62b01f6354138cfbc8f6846c04
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-13 18:28:08 +0530

    array 12/13/2024

---
+++ array.js
@@ -0,0 +1,29 @@
+
+let data = [11,22,33,44,55];
+
+
+// push method = (add data to last index)
+
+// data.push(66,77,88);
+
+// pop method = (remove data to last index)
+
+// data.pop()
+// data.pop()
+// data.pop()
+
+// shift method = (remove data to first index)
+
+// data.shift()
+// data.shift()
+
+// unshift method = (add data to first index) 
+
+// data.unshift(99,78,44,34);
+
+// splice method = add or remove any position
+
+// data.splice(2,3)
+
+
+console.log(data);
