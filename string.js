afffa17d4ea5fa5d6822f832812a7c3db8de7e46
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-17 18:54:29 +0530

    string 12/17/24

---
+++ string.js
@@ -0,0 +1,114 @@
+// string => collection of character 
+
+//using double quots
+// let data = "hello javascrupt";
+
+// let hello = 'hello world';
+
+// let hello = `"hello javascript is world's best using language"`;
+
+// console.log(hello);
+
+
+//srting metohd :=
+
+// // 1). slice
+// let data = "javascript is most populer language"
+
+// let a = data.slice(0,15);
+
+// console.log(a);
+
+// // 2). substring
+// let data = "javascript is most populer language"
+
+// let a = data.substring(0,15);
+
+// console.log(a);
+
+
+// 3). split
+
+// let data = "javascript is most populer language"
+
+// let a = data.split(" ");
+
+// console.log(a);
+
+
+// 4). trim
+
+// let data = "javascript is most populer language"
+
+// let a = data.trim();
+
+// console.log(a.length);
+
+
+// 5). uppercase
+
+// let data = "javascript is most populer language"
+
+// let a = data.toUpperCase();
+
+// console.log(a);
+
+
+// 5). lowercase
+
+// let data = "javascript is most populer language"
+
+// let a = data.toLowerCase();
+
+// console.log(a);
+
+
+
+
+//***************************************************************** */
+
+// math function 
+
+// 1). floor
+
+// let a= Math.to(18.6)
+
+// console.log(a);
+
+
+//2). ceil 
+
+// let a= Math.ceil(18.6)
+
+// console.log(a);
+
+//3). round
+
+// let a= Math.round(18.6)
+
+// console.log(a);
+
+//4). random
+
+// let a= Math.random(18.6)
+
+// console.log(a);
+
+
+
+//********************************************************************** */
+
+// date function 
+
+// let date = new Date;
+
+// console.log(date);
+
+
+
+
+
+
+
+
+
