c5ae0f083e97e33b22cb200a5ba46988911ba2af
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-19 18:44:47 +0530

    object 12/19/24

---
+++ object.js
@@ -0,0 +1,28 @@
+let person = {
+    name: "manthan",
+    age: 19,
+    email:"montu@gmail.com"
+};
+
+// console.log(person);
+
+//dot oprator....
+// .console.log(person.name);
+
+//[bracket]
+// .console.log(perosn["name"]);
+
+// console.log(person.hasOwnProperty["name"]);
+
+// console.log(Object.keys["person"]);
+
+
+//add key value in object
+person.gender = "male";
+console.log(person);
+
+//delete key value in object
+delete person.age;
+console.log(person);
+
+
