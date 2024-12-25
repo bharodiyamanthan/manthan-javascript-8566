beae080b4c36ee6997a42f5f9f0101b080963444
bharodiyamanthan <smitbharodiya007@gmail.com>
2024-12-25 18:57:53 +0530

    dom 12/25/24

---
+++ dom.js
@@ -0,0 +1,55 @@
+// dom => document object Model
+
+// 1). getelementbyid
+//2). getelementbytagname
+//3). getelementby class name
+//4). getelementby query selector
+
+// get element by id
+
+// let h2 = document.getElementById("name");
+
+// console.log(h2);
+
+// h2.innerHTML = "manthan bharodiya";
+// h2.style.color = "red";
+
+/////////////////////////////////////
+
+// getelement tag name
+
+// let data = document.getElementById('p');
+
+// console.log(data);
+
+// data.style.color = "red";
+
+/////////////////////////////////////////////////
+
+// let data = document.getElementsByClassName("manthan");
+
+// console.log(data);
+
+// data.innerHTML = "manthan bharodiya";
+// h2.style.color = "red";
+
+// let data = document.querySelector('h1');
+// console.log(data);
+
+
+let data = document.querySelectorAll('h1');
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
