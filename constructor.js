f7e6ba6d53ce9aed6b4869f44d8e111760fd567e
bharodiyamanthan <smitbharodiya007@gmail.com>
2025-01-01 18:47:36 +0530

    constructor 1/1/25

---
+++ constructor.js
@@ -0,0 +1,15 @@
+class test{
+    constructor(name,city){
+        this.name = name
+        this.city = city
+        console.log("happy new year 2025..............!!!!!!!!!!",this.name,this.city);
+    }
+
+    sayhello(a,b){
+        console.log("addition of a and b:",a+b);
+    }
+}
+
+let manthan = new test("manthan bharodiya","germany");
+
+let obj = new test("manthan bharodiya");
