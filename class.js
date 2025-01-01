9551a68ee3054af5edc5a990472e850992225e40
bharodiyamanthan <smitbharodiya007@gmail.com>
2025-01-01 16:17:37 +0530

    class 1/1/25

---
+++ class.js
@@ -0,0 +1,18 @@
+// class in javascript 
+class hello {
+    sayhello(){
+        console.log("hello manthan bharodiya!!!!!!");
+    }
+
+    addition(a,b){
+        console.log("addition of a and b",a+b);
+    }
+}
+
+
+// object in javascript
+let montu = new hello();
+montu.sayhello();
+montu.addition(10,20);
+
+
